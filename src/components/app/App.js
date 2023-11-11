42c80925758495fb9564bee7b84de30c92568c1b
Benjamin <benjamin.lameko@gmail.com>
2023-11-11 19:35:01 +0300

    Fixed problem with gh-pages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
     
 import TopBar from '../topBar/TopBar';
 import { JobSearchPage, JobPage, FavoritesPage, EmptyStatePage } from '../pages'
